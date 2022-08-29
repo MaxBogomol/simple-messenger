b4d12fb5c644422a4d405419b182aab2774dba90
MaxBogomol <90563409+MaxBogomol@users.noreply.github.com>
2022-08-29 18:18:47 +0500

    Update server.py

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 #server = input("Сервер --> ")
 #port = input("Порт --> ")
 #if server=="":
-server="54.73.53.134"
+server="0.0.0.0"
 #if port=="":
 port=1883
 port=int(port)
