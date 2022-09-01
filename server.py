4f0c016fc754d22922e41c31bbc17064ee8b6758
MaxBogomol <90563409+MaxBogomol@users.noreply.github.com>
2022-09-01 12:33:29 +0500

    Update server.py

--- server.py
+++ server.py
@@ -1,140 +1,224 @@
 import socket
 import select
+import random
+import os
+import json
 
 HEADER_LENGTH = 10
 
-IP = "127.0.0.1"
-PORT = 1234
+IP = "0.0.0.0"
+PORT = 25565
 
-# Create a socket
-# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
-# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+print(socket.gethostbyname(socket.gethostname()))
+
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# SO_ - socket option
-# SOL_ - socket option level
-# Sets REUSEADDR (as a socket option) to 1 on socket
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# Bind, so server informs operating system that it's going to use given IP and port
-# For a server using 0.0.0.0 means to listen on all available interfaces, useful to connect locally to 127.0.0.1 and remotely to LAN interface IP
 server_socket.bind((IP, PORT))
-
-# This makes server listen to new connections
 server_socket.listen()
-
-# List of sockets for select.select()
 sockets_list = [server_socket]
-
-# List of connected clients - socket as a key, user header and name as data
 clients = {}
 
 print(f'Listening for connections on {IP}:{PORT}...')
 
-# Handles message receiving
+class User():
+    def __init__(self, ID):
+        if not os.path.isfile('./users.json'):
+            my_file = open('./users.json', 'w')
+            my_file.write('{}')
+            my_file.close()
+        with open('users.json','r', encoding="utf-8") as f:
+            users = json.load(f)
+            self.username = users[ID]["username"]
+            self.usertag = users[ID]["usertag"]
+            self.password = users[ID]["password"]
+
+            self.username_header = f"{len(self.username.encode('utf-8')):<{HEADER_LENGTH}}".encode('utf-8')
+            self.usertag_header = f"{len(self.usertag.encode('utf-8')):<{HEADER_LENGTH}}".encode('utf-8')
+            self.password_header = f"{len(self.password.encode('utf-8')):<{HEADER_LENGTH}}".encode('utf-8')
+
+    def can_connect(username, usertag, password):
+        connect = None
+        if not os.path.isfile('./users.json'):
+            my_file = open('./users.json', 'w')
+            my_file.write('{}')
+            my_file.close()
+        with open('users.json','r', encoding="utf-8") as f:
+            users = json.load(f)
+            for ID in users.keys():
+                if not ID == "total":
+                    if users[ID]["username"] == username and users[ID]["usertag"] == usertag and users[ID]["password"] == password:
+                        connect = ID
+        return connect
+
+    def can_registration(username):
+        reg = None
+        tags = []
+        for i in range(0, 9999):
+            tags.append(i)
+        if not os.path.isfile('./users.json'):
+            my_file = open('./users.json', 'w')
+            my_file.write('{}')
+            my_file.close()
+        with open('users.json','r', encoding="utf-8") as f:
+            users = json.load(f)
+            for ID in users.keys():
+                if not ID == "total":
+                    if users[ID]["username"] == username:
+                        tags.remove(int(users[ID]["usertag"]))
+                        
+        if len(tags):
+            reg = tags[random.randint(0,len(tags))]
+            usertag_len = 4 - len(str(reg))
+            usertag = "0" * usertag_len
+            usertag = usertag + str(reg)
+            reg = usertag
+
+        return reg
+
+    def registration(username, usertag, password):
+        if not os.path.isfile('./users.json'):
+            my_file = open('./users.json', 'w')
+            my_file.write('{}')
+            my_file.close()
+        with open('users.json','r', encoding="utf-8") as f:
+            users = json.load(f)
+            if not "total" in users:
+                users["total"] = 0
+            ID = users["total"]
+            users["total"] = users["total"] + 1
+            users[ID] = {}
+            users[ID]["username"] = username
+            users[ID]["usertag"] = usertag
+            users[ID]["password"] = password
+        with open('users.json','w', encoding="utf-8") as f:
+            json.dump(users,f)
+        
+
 def receive_message(client_socket):
-
     try:
+        socket_header = client_socket.recv(HEADER_LENGTH)
 
-        # Receive our "header" containing message length, it's size is defined and constant
-        message_header = client_socket.recv(HEADER_LENGTH)
-
-        # If we received no data, client gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
-        if not len(message_header):
+        if not len(socket_header):
             return False
 
-        # Convert header to int value
-        message_length = int(message_header.decode('utf-8').strip())
+        header = int(socket_header.decode('utf-8').strip())
+        if header == 1:
+            username_header = client_socket.recv(HEADER_LENGTH)
+            username_length = int(username_header.decode('utf-8').strip())
+            password_header = client_socket.recv(HEADER_LENGTH)
+            password_length = int(password_header.decode('utf-8').strip())
+            
+            return {'username_header': username_header, 'password_header': password_header,
+                    'username': client_socket.recv(username_length), 'password': client_socket.recv(password_length),
+                    'type': 1}
+        if header == 2:
+            username_header = client_socket.recv(HEADER_LENGTH)
+            username_length = int(username_header.decode('utf-8').strip())
+            usertag_header = client_socket.recv(HEADER_LENGTH)
+            usertag_length = int(usertag_header.decode('utf-8').strip())
+            password_header = client_socket.recv(HEADER_LENGTH)
+            password_length = int(password_header.decode('utf-8').strip())
 
-        # Return an object of message header and message data
-        return {'header': message_header, 'data': client_socket.recv(message_length)}
-
+            return {'username_header': username_header, 'usertag_header': usertag_header, 'password_header': password_header,
+                    'username': client_socket.recv(username_length), 'usertag': client_socket.recv(usertag_length), 'password': client_socket.recv(password_length),
+                    'type': 2}
+        if header == 0:
+            message_header = client_socket.recv(HEADER_LENGTH)
+            message_length = int(message_header.decode('utf-8').strip())
+                
+            return {'header': message_header, 'data': client_socket.recv(message_length),
+                    'type': 0}
+        if header == 100:
+            return {'type': 100}
+        else:
+            return False
     except:
-
-        # If we are here, client closed connection violently, for example by pressing ctrl+c on his script
-        # or just lost his connection
-        # socket.close() also invokes socket.shutdown(socket.SHUT_RDWR) what sends information about closing the socket (shutdown read/write)
-        # and that's also a cause when we receive an empty message
         return False
 
 while True:
-
-    # Calls Unix select() system call or Windows select() WinSock call with three parameters:
-    #   - rlist - sockets to be monitored for incoming data
-    #   - wlist - sockets for data to be send to (checks if for example buffers are not full and socket is ready to send some data)
-    #   - xlist - sockets to be monitored for exceptions (we want to monitor all sockets for errors, so we can use rlist)
-    # Returns lists:
-    #   - reading - sockets we received some data on (that way we don't have to check sockets manually)
-    #   - writing - sockets ready for data to be send thru them
-    #   - errors  - sockets with some exceptions
-    # This is a blocking call, code execution will "wait" here and "get" notified in case any action should be taken
     read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
 
-
-    # Iterate over notified sockets
     for notified_socket in read_sockets:
-
-        # If notified socket is a server socket - new connection, accept it
         if notified_socket == server_socket:
-
-            # Accept new connection
-            # That gives us new socket - client socket, connected to this given client only, it's unique for that client
-            # The other returned object is ip/port set
             client_socket, client_address = server_socket.accept()
-
-            # Client should send his name right away, receive it
             user = receive_message(client_socket)
 
-            # If False - client disconnected before he sent his name
             if user is False:
                 continue
 
-            # Add accepted socket to select.select() list
             sockets_list.append(client_socket)
+            clients[client_socket] = None
 
-            # Also save username and username header
-            clients[client_socket] = user
-
-            print('Accepted new connection from {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
-
-        # Else existing socket is sending a message
+            print('Accepted new connection from {}'.format(client_address))
         else:
-
-            # Receive message
             message = receive_message(notified_socket)
 
-            # If False, client disconnected, cleanup
             if message is False:
-                print('Closed connection from: {}'.format(clients[notified_socket]['data'].decode('utf-8')))
-
-                # Remove from list for socket.socket()
+                #print('Closed connection from: {}#{}'.format(clients[notified_socket]['username'].decode('utf-8'), clients[notified_socket]['usertag'].decode('utf-8')))
+                print('Closed connection from: {}'.format(client_address))
                 sockets_list.remove(notified_socket)
-
-                # Remove from our list of users
                 del clients[notified_socket]
-
                 continue
 
-            # Get user by notified socket, so we will know who sent the message
             user = clients[notified_socket]
+            if user == None:
+                if message['type'] == 1:
+                    username = message['username'].decode('utf-8')
+                    password = message['password'].decode('utf-8')
+                
+                    usertag = User.can_registration(username)
+                    if not usertag == None:
+                        User.registration(username, usertag, password)
+                        ID = User.can_connect(username, usertag, password)
+                        clients[notified_socket] = User(ID)
 
-            print(f'Received message from {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+                        usertag = usertag.encode('utf-8')
+                        connect = "1".encode('utf-8')
+                        socket_header = f"{1:<{HEADER_LENGTH}}".encode('utf-8')
+                        connect_header = f"{len(connect):<{HEADER_LENGTH}}".encode('utf-8')
+                        usertag_header = f"{len(usertag):<{HEADER_LENGTH}}".encode('utf-8')
+                        client_socket.send(socket_header + connect_header + usertag_header + connect + usertag)
 
-            # Iterate over connected clients and broadcast message
-            for client_socket in clients:
+                        print('New registration from {}:{}, username: {}#{}'.format(*client_address, username, usertag))
+                    else:
+                        usertag = usertag.encode('utf-8')
+                        connect = "0".encode('utf-8')
+                        socket_header = f"{1:<{HEADER_LENGTH}}".encode('utf-8')
+                        connect_header = f"{len(connect):<{HEADER_LENGTH}}".encode('utf-8')
+                        usertag_header = f"{len(usertag):<{HEADER_LENGTH}}".encode('utf-8')
+                        
+                        client_socket.send(socket_header + connect_header + usertag_header + connect + usertag)
+                        
+                if message['type'] == 2:
+                    username = message['username'].decode('utf-8')
+                    usertag = message['usertag'].decode('utf-8')
+                    password = message['password'].decode('utf-8')
+                    
+                    ID = User.can_connect(username, usertag, password)
+                    if not ID == None:
 
-                # But don't sent it to sender
-                if client_socket != notified_socket:
+                        clients[notified_socket] = User(ID)
 
-                    # Send user and message (both with their headers)
-                    # We are reusing here message header sent by sender, and saved username header send by user when he connected
-                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+                        connect = "1".encode('utf-8')
+                        socket_header = f"{2:<{HEADER_LENGTH}}".encode('utf-8')
+                        connect_header = f"{len(connect):<{HEADER_LENGTH}}".encode('utf-8')
+                        client_socket.send(socket_header + connect_header + connect)
+                        
+                        print('New login from {}:{}, username: {}#{}'.format(*client_address, username, usertag))
+                    else:
+                        connect = "0".encode('utf-8')
+                        socket_header = f"{2:<{HEADER_LENGTH}}".encode('utf-8')
+                        connect_header = f"{len(connect):<{HEADER_LENGTH}}".encode('utf-8')
+                        client_socket.send(socket_header + connect_header + connect)
+                    
+            else:
+                print(f'Received message from {user.username}#{user.usertag}: {message["data"].decode("utf-8")}')
 
-    # It's not really necessary to have this, but will handle some socket exceptions just in case
+                for client_socket in clients:
+                    if client_socket != notified_socket:
+                        socket_header = f"{0:<{HEADER_LENGTH}}".encode('utf-8')
+                        client_socket.send(socket_header + user.username_header + user.usertag_header + user.username.encode('utf-8') + user.usertag.encode('utf-8') + message['header'] + message['data'])
+
     for notified_socket in exception_sockets:
-
-        # Remove from list for socket.socket()
         sockets_list.remove(notified_socket)
-
-        # Remove from our list of users
         del clients[notified_socket]
